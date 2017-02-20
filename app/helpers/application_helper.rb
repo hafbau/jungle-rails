38f1c9db782f230902ea325933370616a1dfd296
Hafiz Suara <hafbau@yahoo.com>
2017-02-20 17:52:56 +0000

    rating done, rspec installed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avg_by_child_hash_key(arr, key)
+    sum = arr.reduce(0) { |sum, hash| sum + hash[key] }
+    avg = sum / arr.size if arr.size > 0
+    avg ||= 0
+  end
 end
